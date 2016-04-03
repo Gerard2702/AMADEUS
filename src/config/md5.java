23abfd07d0985b7ffb9cdc8a2647bea9c93f386c
cheperecinos93 <rodrigoaguilucho93@gmail.com>
2016-04-03 15:21:46 -0600

    Clase MD5
    Es irreversible, el dato se debe cifrar y comparar con lo que se guardo
    en la BD

---
+++ src/config/md5.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package config;
+import java.security.MessageDigest; 
+import java.security.NoSuchAlgorithmException; 
+/**
+ *
+ * @author Familia Aparicio
+ */
+public class md5 {
+    //CONVERSION A HEXADECIMAL
+    private static String toHexadecimal(byte[] digest){
+        String hash = "";
+        for(byte aux : digest) {
+            int b = aux & 0xff;
+            if (Integer.toHexString(b).length() == 1) hash += "0";
+            hash += Integer.toHexString(b);
+        }
+        return hash;
+    }
+    //MD5 ENCODE STRING
+    public static String md5_encode(String message){
+        byte[] digest = null;
+        byte[] buffer = message.getBytes();
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+            messageDigest.reset();
+            messageDigest.update(buffer);
+            digest = messageDigest.digest();
+        } catch (NoSuchAlgorithmException ex) {
+            System.out.println("Error creando Cifrado MD5");
+        }
+        return toHexadecimal(digest);
+    }
+}
