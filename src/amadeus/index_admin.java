5023fc726188d28f7da11b6a3fda1ed5796100b4
cheperecinos93 <rodrigoaguilucho93@gmail.com>
2016-04-03 21:05:04 -0600

    Cambios a index
    Solo agregar instancia de sus clases

--- src/amadeus/index_admin.java
+++ src/amadeus/index_admin.java
@@ -15,13 +15,20 @@ import javax.swing.*;
  * @author Familia Aparicio
  */
 public class index_admin extends JFrame {
-
+    
+    private JPanel indexadmin;
+    private JLabel imglogo;
+    private JButton reserva,vuelos,usuarios;
+    private ImageIcon iconlogo=new ImageIcon(this.getClass().getResource("/config/logo.png"));   
+    private ImageIcon iconavion=new ImageIcon(this.getClass().getResource("/config/avion.png")); 
+    private ImageIcon iconlista=new ImageIcon(this.getClass().getResource("/config/lista.png")); 
+    private ImageIcon iconuser=new ImageIcon(this.getClass().getResource("/config/user.png")); 
     /**
      * @param args the command line arguments
      */
     public index_admin(){
         initComponent();        
-        this.setSize(500,460);
+        this.setSize(500,480);
         this.setLocationRelativeTo(null);
         this.setTitle("Amadeus Administrador");
         setLayout(null);
@@ -31,11 +38,67 @@ public class index_admin extends JFrame {
     }
     
     public void initComponent(){
+        
+        indexadmin=new JPanel();
+        indexadmin.setBackground(new Color(230,230,230));
+        indexadmin.setBounds(40, 40, 420, 370);
+        indexadmin.setLayout(null);
+        
+        imglogo=new JLabel();
+        imglogo.setBounds(10, 10, 263, 65);
+        imglogo.setIcon(iconlogo);
+        indexadmin.add(imglogo);
+        
+        vuelos=new JButton("Gestión de Vuelos");
+        vuelos.setIcon(iconavion);
+        vuelos.setBounds(100, 95, 210, 60);
+        indexadmin.add(vuelos);
+        
+        reserva=new JButton("Gestión de Reservas");
+        reserva.setIcon(iconlista);
+        reserva.setBounds(100, 175, 210, 60);
+        indexadmin.add(reserva);
+        
+        usuarios=new JButton("Gestión de Usuarios");
+        usuarios.setIcon(iconuser);
+        usuarios.setBounds(100, 255, 210, 60);
+        indexadmin.add(usuarios);
+        
+        add(indexadmin);
+        
+        vuelos.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                modulo_vuelos();
+            }
+        });
+        reserva.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                modulo_reservas();
+            }
+        });
+        usuarios.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                modulo_usuarios();
+            }
+        });
+        
         addWindowListener(new WindowAdapter(){
            public void windowClosing(WindowEvent evt){
                salirmodulo();
            }          
        });
+    }
+    
+    public void modulo_vuelos(){
+        //INSTANCIAR CLASE DE MODULO DE VUELOS
+    }
+    
+    public void modulo_reservas(){
+        //INSTANCIAR CLASE DE MODULO DE RESERVAS
+    }
+    
+    public void modulo_usuarios(){
+        //INSTANCIAR CLASE DE MODULO DE USUARIOS
     }
     
     public void salirmodulo(){
--- src/amadeus/index_user.java
+++ src/amadeus/index_user.java
@@ -15,13 +15,19 @@ import javax.swing.*;
  * @author Familia Aparicio
  */
 public class index_user extends JFrame{
-
+    
+    private JPanel indexadmin;
+    private JLabel imglogo;
+    private JButton reserva,vuelos,usuarios;
+    private ImageIcon iconlogo=new ImageIcon(this.getClass().getResource("/config/logo.png"));   
+    private ImageIcon iconavion=new ImageIcon(this.getClass().getResource("/config/avion.png")); 
+    private ImageIcon iconlista=new ImageIcon(this.getClass().getResource("/config/lista.png")); 
     /**
      * @param args the command line arguments
      */
     public index_user(){
         initComponent();        
-        this.setSize(500,460);
+        this.setSize(500,480);
         this.setLocationRelativeTo(null);
         this.setTitle("Amadeus empleado");
         setLayout(null);
@@ -31,12 +37,54 @@ public class index_user extends JFrame{
     }
     
     public void initComponent(){
+        
+        indexadmin=new JPanel();
+        indexadmin.setBackground(new Color(230,230,230));
+        indexadmin.setBounds(40, 40, 420, 370);
+        indexadmin.setLayout(null);
+        
+        imglogo=new JLabel();
+        imglogo.setBounds(10, 10, 263, 65);
+        imglogo.setIcon(iconlogo);
+        indexadmin.add(imglogo);
+        
+        vuelos=new JButton("Gestión de Vuelos");
+        vuelos.setIcon(iconavion);
+        vuelos.setBounds(100, 95, 210, 60);
+        indexadmin.add(vuelos);
+        
+        reserva=new JButton("Gestión de Reservas");
+        reserva.setIcon(iconlista);
+        reserva.setBounds(100, 175, 210, 60);
+        indexadmin.add(reserva);
+        
+        add(indexadmin);
+        
+        vuelos.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                modulo_vuelos();
+            }
+        });
+        reserva.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                modulo_reservas();
+            }
+        });
+        
         addWindowListener(new WindowAdapter(){
            public void windowClosing(WindowEvent evt){
                salirmodulo();
            }          
        });
     }
+    
+    public void modulo_vuelos(){
+        //INSTANCIAR CLASE DE MODULO DE VUELOS
+    }
+    
+    public void modulo_reservas(){
+        //INSTANCIAR CLASE DE MODULO DE RESERVAS
+    }   
     
     public void salirmodulo(){
         int dialogResult = JOptionPane.showConfirmDialog (rootPane, "¿Esta seguro de salir de aplicación?","Cerrar Sesión",JOptionPane.YES_NO_OPTION);
