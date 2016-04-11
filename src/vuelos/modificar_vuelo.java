b673f20a24a7a029e45d5de9027e3587fd268e54
DarkXav <rogel1993@gmail.com>
2016-04-11 04:34:40 -0600

    Modificacion de Vuelo
    formulario datos de modificacion de vuelo

--- src/vuelos/modificar_vuelo.java
+++ src/vuelos/modificar_vuelo.java
@@ -4,16 +4,22 @@
  * and open the template in the editor.
  */
 package vuelos;
+import amadeus.login;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import config.*;
 import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.table.AbstractTableModel;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableCellRenderer;
+import javax.swing.text.MaskFormatter;
 
 /**
  *
@@ -21,7 +27,10 @@ import javax.swing.table.TableCellRenderer;
  */
 public class modificar_vuelo extends JPanel
 {
-    private JLabel lblTitulo;
+    private JLabel lblTitulo, lblhora1, lblhora2, lblruta, lblavion, lblfecha, x;
+    private JComboBox cbxRuta, cbxAvion;
+    private JFormattedTextField txtHora1, txtHora2, txtFecha;
+    private JButton btnModificar;
     private JSeparator sep;
     private JScrollPane jScrollPane1;
     private JTable jTable;
@@ -75,9 +84,145 @@ public class modificar_vuelo extends JPanel
         sep.setFont(label);
         sep.setBounds(10, 50, 685, 5);
         sep.setForeground(new Color(220,220,220));
-        add(sep);
-        
-        
+        add(sep);  
+            
+            lblhora1 = new JLabel("Hora Inicio: ");
+            lblhora1.setFont(label);
+            lblhora1.setBounds(30,150,100,25);
+            add(lblhora1);
+
+            MaskFormatter mascara2 = null;
+            try { 
+                mascara2 = new MaskFormatter("##:##"); 
+                } catch (ParseException ex) { 
+            }
+            txtHora1 = new JFormattedTextField(mascara2);
+            txtHora1.setBounds(150,150,250,25);            
+            add(txtHora1);
+
+            lblhora2 = new JLabel("Hora LLegada: ");
+            lblhora2.setFont(label);
+            lblhora2.setBounds(30,190,100,25);
+            add(lblhora2);
+
+            txtHora2 = new JFormattedTextField(mascara2);
+            txtHora2.setBounds(150,190,250,25);            
+            add(txtHora2);
+
+            lblfecha = new JLabel("Fecha: ");
+            lblfecha.setFont(label);
+            lblfecha.setBounds(30,230,100,25);
+            add(lblfecha);
+
+            MaskFormatter mascara = null;
+            try { 
+                mascara = new MaskFormatter("####-##-##"); 
+                } catch (ParseException ex) { 
+            }
+            txtFecha = new JFormattedTextField(mascara);
+            txtFecha.setBounds(150,230,250,25);
+            add(txtFecha);
+
+            lblavion = new JLabel("Avion: ");
+            lblavion.setFont(label);
+            lblavion.setBounds(30,270,100,25);
+            add(lblavion);
+
+            cbxAvion = new JComboBox();
+            cbxAvion.setBounds(150,270,250,25);
+            add(cbxAvion);
+
+            lblruta = new JLabel("Ruta: ");
+            lblruta.setFont(label);
+            lblruta.setBounds(30,310,100,25);
+            add(lblruta);
+
+            cbxRuta = new JComboBox();
+            cbxRuta.setBounds(150,310,250,25);
+            add(cbxRuta);
+
+            btnModificar = new JButton("Modificar");
+            btnModificar.setBounds(30,360,200,25);
+            btnModificar.setBackground(new Color(158,203,242));        
+            btnModificar.setBorderPainted(false);
+            btnModificar.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseEntered(MouseEvent me) {
+                    btnModificar.setBackground(new Color(200,200,200));
+                }
+                @Override
+                public void mouseExited(MouseEvent e) {
+                    btnModificar.setBackground(new Color(158,203,242));
+                }            
+            });
+            add(btnModificar);
+            btnModificar.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent evt){
+                modificar_datos_vuelo(idvuelo);
+            }
+        });
+        try{
+            db.conectar();
+            int idvuelo2 = Integer.parseInt(idvuelo);
+            String sql = "SELECT * FROM vuelos WHERE idvuelos='"+idvuelo2+"';";
+            ResultSet sd = db.query(sql);
+            sd.first();            
+            String fecha = sd.getString("fecha");
+            int idavion = sd.getInt("avion_idavion");
+            int idruta = sd.getInt("ruta_idruta"); 
+            String hora1 = sd.getString("hora_inicio");
+            String hora2 = sd.getString("hora_fin");
+            String sql2 = "SELECT * FROM ruta INNER JOIN aeropuertos WHERE ruta.idruta='"+idruta+"' AND aeropuertos.idaeropuertos=ruta.destino;";
+            ResultSet sd2 = db.query(sql2);
+            sd2.first();
+            String destino = sd2.getString("ciudad");
+            String sql3 = "SELECT * FROM avion WHERE idavion='"+idavion+"';";
+            ResultSet sd3 = db.query(sql3);
+            sd3.first();
+            String codavion = sd3.getString("codigo");
+            txtHora1.setText(hora1);
+            txtHora2.setText(hora2);
+            txtFecha.setText(fecha);
+            cbxAvion.addItem(codavion);
+            cbxAvion.setSelectedIndex(0);
+            int indiceruta = 0, rutafinal = 0;
+            String sql4="SELECT avion.codigo FROM avion WHERE idavion NOT IN (SELECT idavion FROM vuelos WHERE avion.idavion = vuelos.avion_idavion) ORDER BY idavion";
+            ResultSet rs = db.query(sql4);
+            rs.last(); 
+            if(rs.getRow()==0){
+                cbxAvion.addItem("NO HAY AVIONES AGREGADOS");
+            }
+            else{
+                rs.beforeFirst();
+                while(rs.next()){                    
+                    String idavion2=rs.getString("codigo");
+                     cbxAvion.addItem(idavion2);
+                }
+            }
+            String sql5="SELECT * FROM aeropuertos";
+            ResultSet rs2 = db.query(sql5);
+            rs2.last(); 
+            if(rs2.getRow()==0){
+                cbxRuta.addItem("NO HAY RUTAS AGREGADOS");
+            }
+            else{
+                rs2.beforeFirst();
+                while(rs2.next()){
+                String cid=rs2.getString("ciudad");
+                if(cid.equals(destino))
+                {
+                    rutafinal = indiceruta;
+                    cbxRuta.addItem("San Salvador - "+cid);
+                }
+                else{
+                    indiceruta++;
+                    cbxRuta.addItem("San Salvador - "+cid);
+                }
+                }
+            }
+            cbxRuta.setSelectedIndex(rutafinal);
+            db.desconectar();   
+        }catch(SQLException e){}
     }
     
         private void setTabla() {
@@ -218,5 +363,71 @@ public class modificar_vuelo extends JPanel
         catch(Exception e){
            JOptionPane.showMessageDialog(null, "Seleccionada la fila "+ e); 
         }            
-    }  
+    }
+        
+    private void modificar_datos_vuelo(String idvuelo)
+    {
+        String fecha = this.txtFecha.getText().toString();
+        String horainicio = this.txtHora1.getText().toString();
+        String horafin = this.txtHora2.getText().toString();
+        String avion = this.cbxAvion.getSelectedItem().toString();
+        String rutaorigen = "SAL";
+        String rutadestinoop = this.cbxRuta.getSelectedItem().toString();
+        String rutadestino = rutadestinoop.substring(15);
+        int idvl = Integer.parseInt(idvuelo);
+        if(isFechaValida(fecha) && isHoraValida(horainicio) && isHoraValida(horafin))
+        {
+            try{
+                    db.conectar();
+                    
+                    String sqlidestino = "SELECT * FROM aeropuertos WHERE ciudad='"+rutadestino+"';";
+                    ResultSet iddesti = db.query(sqlidestino);
+                    iddesti.first();                    
+                    String idru = iddesti.getString("idaeropuertos");                    
+                    
+                    String sqlid="SELECT * FROM ruta WHERE origen='"+rutaorigen+"' AND destino='"+idru+"';";
+                    ResultSet rsid = db.query(sqlid);
+                    rsid.first();
+                    int idruta = rsid.getInt("idruta");
+                    
+                    String sqlmale ="SELECT * FROM avion WHERE codigo='"+avion+"';";
+                    ResultSet mal = db.query(sqlmale);
+                    mal.first();
+                    String maletasdis = mal.getString("maletas");
+                    String asientosdis = mal.getString("asientos");
+                    int idmod = mal.getInt("modelo_idmodelo");
+                    int idavion = mal.getInt("idavion");                    
+                    
+                    String sqlupdate="UPDATE vuelos SET hora_inicio='"+horainicio+"', hora_fin='"+horafin+"',fecha='"+fecha+"',asientos_disponibles='"+asientosdis+"',maletas_disponibles='"+maletasdis+"',avion_idavion='"+idavion+"',avion_modelo_idmodelo='"+idmod+"',ruta_idruta='"+idruta+"'WHERE idvuelos='"+idvl+"';";
+                    db.queryUpdate(sqlupdate);
+                    JOptionPane.showMessageDialog(null,"REGISTRO EXITOSO");                    
+                    db.desconectar();
+                    }catch (SQLException ex) {
+                        Logger.getLogger(login.class.getName()).log(Level.SEVERE, null, ex);
+                    } 
+        }
+    }
+    
+     public static boolean isFechaValida(String fecha) {
+        try {
+            SimpleDateFormat formatoFecha = new SimpleDateFormat("yyyy-MM-dd");
+            formatoFecha.setLenient(false);
+            formatoFecha.parse(fecha);
+        }catch(ParseException e) {
+            return false;
+        }
+        return true;
+    }
+     
+    public static boolean isHoraValida(String hora) {
+            SimpleDateFormat formatohora = new SimpleDateFormat("HH:mm");
+            formatohora.setLenient(false);
+            try{
+                formatohora.parse(hora);
+            }catch(ParseException e)            
+            {
+             return false;
+            }
+            return true;
+    }
 }
